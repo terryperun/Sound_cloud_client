bd9cc8f60f9ec9abc68b98c719a5d65d446fb5fb
Pikser <32457085+Pikser@users.noreply.github.com>
2018-08-25 20:21:07 +0300

    refactor components Stream

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
-import Stream from './components/Stream';
+import Stream from './components/Stream/index';
 // import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 const tracks = [
   {
-    title: 'Some track'
+    title: 'Some track',
   },
   {
-    title: 'Some ather track'
-  }
+    title: 'Some ather track',
+  },
 ];
 
 const store = configureStore();
 store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
-  <Stream />,
-  document.getElementById('root'));
+  <Provider store={store}>
+    <Stream />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
